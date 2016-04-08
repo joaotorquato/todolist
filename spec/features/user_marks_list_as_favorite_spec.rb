11f07ee7078834cdd7c5c4a357bc92caec9e13b8
João Torquato <joao.otl@gmail.com>
2016-04-08 15:14:55 -0300

    change links to buttons * change link to button on partial _list.html.erb * change checkbox to button on partial _task.html.erb

--- spec/features/user_marks_list_as_favorite_spec.rb
+++ spec/features/user_marks_list_as_favorite_spec.rb
@@ -10,15 +10,13 @@ feature 'User marks others\' public list as favorite' do
     visit public_lists_path
 
     within "#list-#{other_user_list.id}" do
-      favorite_link = mark_favorite_list_path(other_user_list)
-      expect(page).to have_link 'Mark as favorite', href: favorite_link
       click_on 'Mark as favorite'
     end
 
     expect(page.current_path).to eq public_lists_path
     expect(page).to have_content 'List was mark as favorite successfuly.'
     within "#list-#{other_user_list.id}" do
-      expect(page).to have_link 'Unmark as favorite'
+      expect(page).to have_button 'Unmark as favorite'
     end
   end
 end
